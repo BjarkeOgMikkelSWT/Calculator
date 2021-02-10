e05250cf948d2df19d7c09e3d6192338518eaa3d
Mikkel Kirkegaard <fisk9889@gmail.com>
2021-02-10 09:39:46 +0100

    Test of webhook

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -12,6 +12,7 @@ namespace Hand_Testing_Calculator
             Console.WriteLine("Sub -3 - 7: {0}", calc.Subtract(-3,7));
             Console.WriteLine("Multiply 42 x 69: {0}", calc.Multiply(42, 69));
             Console.WriteLine("Test of 2 to the power of 7: {0}", calc.Power(2, 7));
+            Console.WriteLine("Hello world!");
         }
     }
 }
